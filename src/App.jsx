2fc8bd16aacb58e1c6e1734f0258dfff1189b48a
coderNishant11 <mr.nishantksrivastava@gmail.com>
2025-01-25 00:24:20 +0530

    used tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <header className="logo">
-          <Link to="/">Adopt Me</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <header className="logo">
+            <Link to="/">Adopt Me</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </div>
   );
